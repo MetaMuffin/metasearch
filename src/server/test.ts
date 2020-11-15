76cfa42a0fe902012730b58e4f9499c8a8843048
Lars Lenckowski <larslenckowski@gmail.com>
2020-11-15 16:06:03 +0100

    add add_word method

--- src/server/test.ts
+++ src/server/test.ts
@@ -3,7 +3,11 @@ import { Metadb } from "./database"
 var db = new Metadb();
 
 (async () => {
+	console.log("Testing database stuff")
+
 	await db.init()
 	console.log(await db.word_freq("Terror"))
+	console.log(await db.add_word("wordthatshouldntalreadybeinthedatabase", 5))
+	console.log(await db.add_word("Terror", 1))
 	await db.close()
 })();
